cd6440b197e733f39850da8804f3caad5072bfcb
r.peltretti <r.peltretti@xeel.tech>
2025-12-20 18:08:53 +0100

    fixed gx navigation for nvim cli (windows)

--- init.lua
+++ init.lua
@@ -199,6 +199,38 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+vim.keymap.set('n', 'gx', function()
+  local target = vim.fn.expand '<cfile>'
+
+  local nvim_extensions = {
+    md = true,
+    txt = true,
+    log = true,
+    cs = true,
+    py = true,
+    json = true,
+  }
+
+  -- helper: extract extension
+  local function get_ext(path)
+    return path:match '^.+%.([^./\\#]+)' -- ignores anchors
+  end
+
+  -- helper: strip markdown anchors
+  local ext = get_ext(target:gsub('#.*$', ''))
+
+  -- use Neovim only if:
+  --   - no extension
+  --   - or extension explicitly allowed
+  if ext == nil or nvim_extensions[ext:lower()] then
+    vim.cmd('edit ' .. vim.fn.fnameescape(target))
+    return
+  end
+
+  -- otherwise, delegate to OS default
+  vim.fn.jobstart({ 'cmd', '/c', 'start', target }, { detach = true })
+end, { silent = true })
+
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
 -- vim.keymap.set("n", "<C-S-h>", "<C-w>H", { desc = "Move window to the left" })
 -- vim.keymap.set("n", "<C-S-l>", "<C-w>L", { desc = "Move window to the right" })
