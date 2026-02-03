40665dcfa91f73f3caf7f03fa35df2c627c9c1a3
Peltrettir <peltrettir@gmail.com>
2026-02-04 00:37:18 +0100

    spacing and peltrettir/easy-dotnet.nvim

--- lua/custom/plugins/easy-dotnet.lua
+++ lua/custom/plugins/easy-dotnet.lua
@@ -1,89 +1,165 @@
 local function add_dotnet_mappings()
-  local dotnet = require 'easy-dotnet'
+    local dotnet = require 'easy-dotnet'
 
-  vim.api.nvim_create_user_command('Secrets', function()
-    dotnet.secrets()
-  end, {})
+    vim.api.nvim_create_user_command('Secrets', function()
+        dotnet.secrets()
+    end, {})
 
-  vim.keymap.set('n', '<A-t>', function()
-    vim.cmd 'Dotnet testrunner'
-  end, { nowait = true })
+    vim.keymap.set('n', '<A-t>', function()
+        vim.cmd 'Dotnet testrunner'
+    end, { nowait = true })
 
-  vim.keymap.set('n', '<A-p>', function()
-    vim.cmd 'Dotnet project view'
-  end, { nowait = true })
+    vim.keymap.set('n', '<A-p>', function()
+        vim.cmd 'Dotnet project view'
+    end, { nowait = true })
 
-  vim.keymap.set('n', '<C-p>', function()
-    vim.cmd 'Dotnet debug profile default'
-  end, { nowait = true })
+    vim.keymap.set('n', '<C-p>', function()
+        vim.cmd 'Dotnet debug profile default'
+    end, { nowait = true })
 
-  vim.keymap.set('n', '<c-b>', function()
-    dotnet.build_default_quickfix()
-  end, { nowait = true })
+    vim.keymap.set('n', '<c-b>', function()
+        dotnet.build_default_quickfix()
+    end, { nowait = true })
 end
 
 return {
-  {
-    'gustaveikaas/easy-dotnet.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-    config = function()
-      local dotnet = require 'easy-dotnet'
-      dotnet.setup {
-        ---@type easy-dotnet.TestRunner.Options
-        test_runner = {
-          enable_buffer_test_execution = true,
-          viewmode = 'float',
-          mappings = {
-            debug_test_from_buffer = { lhs = '<leader>d', desc = 'debug test from buffer' },
-          },
-        },
-        projx_lsp = {
-          enabled = true,
-        },
-        notifications = {
-          handler = false,
-        },
-        debugger = {
-          bin_path = nil,
-          apply_value_converters = true,
-          auto_register_dap = true,
-          mappings = {
-            open_variable_viewer = { lhs = 'T', desc = 'open variable viewer' },
-          },
-        },
-        auto_bootstrap_namespace = {
-          type = 'file_scoped',
-          enabled = true,
-        },
-        server = {
-          use_visual_studio = false,
-          ---@type nil | "Off" | "Critical" | "Error" | "Warning" | "Information" | "Verbose" | "All"
-          log_level = 'Off',
-        },
-        terminal = function(path, action, args, ctx)
-          local commands = {
-            run = function()
-              return string.format('%s %s', ctx.cmd, args)
-            end,
-            test = function()
-              return string.format('%s %s', ctx.cmd, args)
-            end,
-            restore = function()
-              return string.format('%s %s', ctx.cmd, args)
-            end,
-            build = function()
-              return string.format('%s %s', ctx.cmd, args)
-            end,
-            watch = function()
-              return string.format('dotnet watch --project %s %s', path, args)
-            end,
-          }
-
-          local command = commands[action]() .. '\r'
-          require('toggleterm').exec(command, nil, nil, nil, 'float')
+    {
+        'Peltrettir/easy-dotnet.nvim',
+        branch = 'develop',
+        dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
+        config = function()
+            local dotnet = require 'easy-dotnet'
+            dotnet.setup {
+                lsp = {
+                    enabled = true, -- Enable builtin roslyn lsp
+                    roslynator_enabled = true, -- Automatically enable roslynator analyzer
+                    easy_dotnet_analyzer_enabled = true, -- Enable roslyn analyzer from easy-dotnet-server
+                    analyzer_assemblies = {}, -- Any additional roslyn analyzers you might use like SonarAnalyzer.CSharp
+                    config = {},
+                },
+                debugger = {
+                    -- Path to custom coreclr DAP adapter
+                    -- easy-dotnet-server falls back to its own netcoredbg binary if bin_path is nil
+                    bin_path = 'C:\\Users\\utente\\AppData\\Local\\nvim-data\\mason\\packages\\netcoredbg\\netcoredbg\\netcoredbg.exe',
+                    apply_value_converters = true,
+                    auto_register_dap = true,
+                    mappings = {
+                        open_variable_viewer = { lhs = 'T', desc = 'open variable viewer' },
+                    },
+                },
+                ---@type TestRunnerOptions
+                test_runner = {
+                    ---@type "split" | "vsplit" | "float" | "buf"
+                    viewmode = 'float',
+                    ---@type number|nil
+                    vsplit_width = nil,
+                    ---@type string|nil "topleft" | "topright"
+                    vsplit_pos = nil,
+                    enable_buffer_test_execution = true, --Experimental, run tests directly from buffer
+                    noBuild = true,
+                    icons = {
+                        passed = '',
+                        skipped = '',
+                        failed = '',
+                        success = '',
+                        reload = '',
+                        test = '',
+                        sln = '󰘐',
+                        project = '󰘐',
+                        dir = '',
+                        package = '',
+                    },
+                    mappings = {
+                        run_test_from_buffer = { lhs = '<leader>r', desc = 'run test from buffer' },
+                        run_all_tests_from_buffer = { lhs = '<leader>t', desc = 'run all tests from buffer' },
+                        peek_stack_trace_from_buffer = { lhs = '<leader>p', desc = 'peek stack trace from buffer' },
+                        filter_failed_tests = { lhs = '<leader>fe', desc = 'filter failed tests' },
+                        debug_test = { lhs = '<leader>d', desc = 'debug test' },
+                        go_to_file = { lhs = 'g', desc = 'go to file' },
+                        run_all = { lhs = '<leader>R', desc = 'run all tests' },
+                        run = { lhs = '<leader>r', desc = 'run test' },
+                        peek_stacktrace = { lhs = '<leader>p', desc = 'peek stacktrace of failed test' },
+                        expand = { lhs = 'o', desc = 'expand' },
+                        expand_node = { lhs = 'E', desc = 'expand node' },
+                        expand_all = { lhs = '-', desc = 'expand all' },
+                        collapse_all = { lhs = 'W', desc = 'collapse all' },
+                        close = { lhs = 'q', desc = 'close testrunner' },
+                        refresh_testrunner = { lhs = '<C-r>', desc = 'refresh testrunner' },
+                    },
+                    --- Optional table of extra args e.g "--blame crash"
+                    additional_args = {},
+                },
+                new = {
+                    project = {
+                        prefix = 'sln', -- "sln" | "none"
+                    },
+                },
+                ---@param action "test" | "restore" | "build" | "run"
+                terminal = function(path, action, args)
+                    args = args or ''
+                    local commands = {
+                        run = function()
+                            return string.format('dotnet run --project %s %s', path, args)
+                        end,
+                        test = function()
+                            return string.format('dotnet test %s %s', path, args)
+                        end,
+                        restore = function()
+                            return string.format('dotnet restore %s %s', path, args)
+                        end,
+                        build = function()
+                            return string.format('dotnet build %s %s', path, args)
+                        end,
+                        watch = function()
+                            return string.format('dotnet watch --project %s %s', path, args)
+                        end,
+                    }
+                    local command = commands[action]()
+                    if require('easy-dotnet.extensions').isWindows() == true then
+                        command = command .. '\r'
+                    end
+                    vim.cmd 'vsplit'
+                    vim.cmd('term ' .. command)
+                end,
+                csproj_mappings = true,
+                fsproj_mappings = true,
+                auto_bootstrap_namespace = {
+                    --block_scoped, file_scoped
+                    type = 'block_scoped',
+                    enabled = true,
+                    use_clipboard_json = {
+                        behavior = 'prompt', --'auto' | 'prompt' | 'never',
+                        register = '+', -- which register to check
+                    },
+                },
+                server = {
+                    ---@type nil | "Off" | "Critical" | "Error" | "Warning" | "Information" | "Verbose" | "All"
+                    log_level = nil,
+                },
+                -- choose which picker to use with the plugin
+                -- possible values are "telescope" | "fzf" | "snacks" | "basic"
+                -- if no picker is specified, the plugin will determine
+                -- the available one automatically with this priority:
+                -- telescope -> fzf -> snacks ->  basic
+                picker = 'telescope',
+                background_scanning = true,
+                notifications = {
+                    --Set this to false if you have configured lualine to avoid double logging
+                    handler = function(start_event)
+                        local spinner = require('easy-dotnet.ui-modules.spinner').new()
+                        spinner:start_spinner(start_event.job.name)
+                        ---@param finished_event JobEvent
+                        return function(finished_event)
+                            spinner:stop_spinner(finished_event.result.msg, finished_event.result.level)
+                        end
+                    end,
+                },
+                diagnostics = {
+                    default_severity = 'error',
+                    setqflist = false,
+                },
+            }
+            add_dotnet_mappings()
         end,
-      }
-      add_dotnet_mappings()
-    end,
-  },
+    },
 }
