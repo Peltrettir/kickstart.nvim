308fce15029d0800f6d131b92b296676414bd6aa
Peltrettir <peltrettir@gmail.com>
2026-03-19 23:05:09 +0100

    added <C-Del> map in insert mode

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -16,50 +16,50 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 vim.keymap.set('n', '<C-down>', function()
-  vim.cmd 'resize -1'
+    vim.cmd 'resize -1'
 end, { desc = 'Resize window reduce height' })
 vim.keymap.set('n', '<C-up>', function()
-  vim.cmd 'resize +1'
+    vim.cmd 'resize +1'
 end, { desc = 'Resize window increase height' })
 vim.keymap.set('n', '<C-left>', function()
-  vim.cmd 'vertical resize -1'
+    vim.cmd 'vertical resize -1'
 end, { desc = 'Resize window reduce width' })
 vim.keymap.set('n', '<C-right>', function()
-  vim.cmd 'vertical resize +1'
+    vim.cmd 'vertical resize +1'
 end, { desc = 'Resize window increase width' })
 
 -- NOTE: Windows default association don't require GUI applications
 -- manually mapped text files to nvim
 vim.keymap.set('n', 'gx', function()
-  local target = vim.fn.expand '<cfile>'
+    local target = vim.fn.expand '<cfile>'
 
-  local nvim_extensions = {
-    md = true,
-    txt = true,
-    log = true,
-    cs = true,
-    py = true,
-    json = true,
-  }
+    local nvim_extensions = {
+        md = true,
+        txt = true,
+        log = true,
+        cs = true,
+        py = true,
+        json = true,
+    }
 
-  -- helper: extract extension
-  local function get_ext(path)
-    return path:match '^.+%.([^./\\#]+)' -- ignores anchors
-  end
+    -- helper: extract extension
+    local function get_ext(path)
+        return path:match '^.+%.([^./\\#]+)' -- ignores anchors
+    end
 
-  -- helper: strip markdown anchors
-  local ext = get_ext(target:gsub('#.*$', ''))
+    -- helper: strip markdown anchors
+    local ext = get_ext(target:gsub('#.*$', ''))
 
-  -- use Neovim only if:
-  --   - no extension
-  --   - or extension explicitly allowed
-  if ext == nil or nvim_extensions[ext:lower()] then
-    vim.cmd('edit ' .. vim.fn.fnameescape(target))
-    return
-  end
+    -- use Neovim only if:
+    --   - no extension
+    --   - or extension explicitly allowed
+    if ext == nil or nvim_extensions[ext:lower()] then
+        vim.cmd('edit ' .. vim.fn.fnameescape(target))
+        return
+    end
 
-  -- otherwise, delegate to OS default
-  vim.fn.jobstart({ 'cmd', '/c', 'start', target }, { detach = true })
+    -- otherwise, delegate to OS default
+    vim.fn.jobstart({ 'cmd', '/c', 'start', target }, { detach = true })
 end, { silent = true })
 
 -- NOTE: Some terminals have colliding keymaps or are not able to send distinct keycodes
@@ -67,3 +67,7 @@ vim.keymap.set('n', '<C-S-h>', '<C-w>H', { desc = 'Move window to the left' })
 vim.keymap.set('n', '<C-S-l>', '<C-w>L', { desc = 'Move window to the right' })
 vim.keymap.set('n', '<C-S-j>', '<C-w>J', { desc = 'Move window to the lower' })
 vim.keymap.set('n', '<C-S-k>', '<C-w>K', { desc = 'Move window to the upper' })
+
+-- NOTE: Insert mode keymaps
+vim.keymap.set("i", "<C-H>", "<esc>dbi", {silent = true, desc = 'delete from cursor to beginning of word'})
+vim.keymap.set("i", "<C-Del>", "<esc>dwi", {silent = true, desc = 'delete from cursor to ending of word'})
