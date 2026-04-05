74e3779106b0646ef60b54a2ec90af214af5573c
Peltrettir <peltrettir@gmail.com>
2026-04-05 16:16:50 +0200

    Added treesitter folding whenever available

--- lua/custom/autocommands.lua
+++ lua/custom/autocommands.lua
@@ -7,25 +7,39 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     end,
 })
 
-vim.api.nvim_create_autocmd('BufWritePost', {
-    desc = 'When saving orgfiles mark reload agenda if not previously existing',
-    pattern = '*.org',
+vim.api.nvim_create_autocmd({ 'FileType', 'BufWinEnter' }, {
+    desc = 'User treesitter folding whenever available',
+    group = vim.api.nvim_create_augroup('LspFolding', { clear = true }),
     callback = function(args)
-        if vim.b.org_file_existed then
-            return
+        local ok = pcall(vim.treesitter.get_parser, args.buf)
+        if ok then
+            vim.api.nvim_buf_call(args.buf, function()
+                vim.wo.foldmethod = 'expr'
+                vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+            end)
         end
-
-        vim.b.org_file_existed = true
-        local orgmode = require 'orgmode'
-        orgmode.files:reload()
-        -- orgmode.superagenda:update_agenda()
     end,
 })
 
-vim.api.nvim_create_autocmd('BufReadPost', {
-    desc = 'When opening orgfiles mark them as existing',
-    pattern = '*.org',
-    callback = function()
-        vim.b.org_file_existed = true
-    end,
-})
+-- vim.api.nvim_create_autocmd('BufWritePost', {
+--     desc = 'When saving orgfiles mark reload agenda if not previously existing',
+--     pattern = '*.org',
+--     callback = function(args)
+--         if vim.b.org_file_existed then
+--             return
+--         end
+--
+--         vim.b.org_file_existed = true
+--         local orgmode = require 'orgmode'
+--         orgmode.files:reload()
+--         -- orgmode.superagenda:update_agenda()
+--     end,
+-- })
+--
+-- vim.api.nvim_create_autocmd('BufReadPost', {
+--     desc = 'When opening orgfiles mark them as existing',
+--     pattern = '*.org',
+--     callback = function()
+--         vim.b.org_file_existed = true
+--     end,
+-- })
